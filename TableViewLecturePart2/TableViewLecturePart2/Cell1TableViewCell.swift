8ba43c54b96f59937ceac0fcc1a2dbd41e5aa60b
HeatherConnery <heatherconnery@gmail.com>
2015-10-29 13:14:44 -0400

    all but last 30 minutes

---
+++ TableViewLecturePart2/TableViewLecturePart2/Cell1TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  Cell1TableViewCell.swift
+//  TableViewLecturePart2
+//
+//  Created by Heather Connery on 2015-10-29.
+//  Copyright © 2015 HConnery. All rights reserved.
+//
+
+import UIKit
+
+class Cell1TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TableViewLecturePart2/TableViewLecturePart2/ViewController.swift
+++ TableViewLecturePart2/TableViewLecturePart2/ViewController.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    enum TableViewCellIdentifiers: String {
+        case FirstCell = "Cell1"
+        case SecondCell = "Cell2"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        var cell = UITableViewCell()
+        
+        if indexPath.row % 2 == 0 {
+            cell = tableView.dequeueReusableCellWithIdentifier(TableViewCellIdentifiers.FirstCell.rawValue, forIndexPath: indexPath)
+        } else {
+            cell = tableView.dequeueReusableCellWithIdentifier(TableViewCellIdentifiers.SecondCell.rawValue, forIndexPath: indexPath)
+        }
+        return cell
+    }
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    
 
 }
 
